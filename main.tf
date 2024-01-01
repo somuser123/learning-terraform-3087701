9f741f64ed956a76e1731b84eb862f920a8bdf47
somuser123 <154913391+somuser123@users.noreply.github.com>
2024-01-01 17:45:21 +0100

    corrected

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
-
+}
 
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
