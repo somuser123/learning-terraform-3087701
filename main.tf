cec63daca1fab33c563a1ef7d29ee0518174b8a9
somuser123 <154913391+somuser123@users.noreply.github.com>
2024-01-01 17:47:35 +0100

    new implementation

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
