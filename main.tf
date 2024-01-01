752d2b59db2a3800ae2be210bd60a7794148b76b
somuser123 <154913391+somuser123@users.noreply.github.com>
2024-01-01 17:43:56 +0100

    new test

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "Some done it "
-  }
+resource "aws_instance" "example" {
+    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
 }
